a0404af4034bca6ce528468f75c03172cf29421c
sambapython@gmail.com <sambapython@gmail.com>
2023-01-19 08:53:24 +0530

    dict set mutable and immutable

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
-a=10
-b=20
-print(a+b)
+import schedule
+import time
+
+def job():
+    print("I'm working...")
+    time.sleep(3)
+    print("done")
+
+schedule.every(1).seconds.do(job)
+time.sleep(3)
+schedule.run_pending()
+while True:
+    for job in schedule.get_jobs():
+        job.run()
+        time.sleep(1)
+
